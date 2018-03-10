55cb556e8a1b262a489bdc1a987ce6ac49d56e21
aweitz <andrew.j.weitz@gmail.com>
2018-03-10 13:24:20 -0800

    FIS

--- code/qa_model.py
+++ code/qa_model.py
@@ -206,20 +206,20 @@ class QAModel(object):
         encoderMod   = RNNEncoder(self.FLAGS.hidden_size, self.keep_prob, self.FLAGS.num_rnn_layers, scope="RNNEncoderMod")
         blended_reps = encoderMod.build_graph(blended_reps, self.context_mask) # (batch_size, context_len, hidden_size*2)
 
-        blended_reps_final = tf.contrib.layers.fully_connected(blended_reps, num_outputs=self.FLAGS.hidden_size, weights_regularizer=tf.contrib.layers.l2_regularizer(scale=self.FLAGS.L2_reg)) # blended_reps_final is shape (batch_size, context_len, hidden_size)
+        blended_reps_final = tf.contrib.layers.fully_connected(blended_reps, num_outputs=self.FLAGS.hidden_size) # blended_reps_final is shape (batch_size, context_len, hidden_size)
         #######################################################
 
         # Use softmax layer to compute probability distribution for start location
         # Note this produces self.logits_start and self.probdist_start, both of which have shape (batch_size, context_len)
         with vs.variable_scope("StartDist"):
             softmax_layer_start = SimpleSoftmaxLayer()
-            self.logits_start, self.probdist_start = softmax_layer_start.build_graph(blended_reps_final, self.context_mask, regularizer=tf.contrib.layers.l2_regularizer(scale=self.FLAGS.L2_reg))
+            self.logits_start, self.probdist_start = softmax_layer_start.build_graph(blended_reps_final, self.context_mask)
 
         # Use softmax layer to compute probability distribution for end location
         # Note this produces self.logits_end and self.probdist_end, both of which have shape (batch_size, context_len)
         with vs.variable_scope("EndDist"):
             softmax_layer_end = SimpleSoftmaxLayer()
-            self.logits_end, self.probdist_end = softmax_layer_end.build_graph(blended_reps_final, self.context_mask, regularizer=tf.contrib.layers.l2_regularizer(scale=self.FLAGS.L2_reg))
+            self.logits_end, self.probdist_end = softmax_layer_end.build_graph(blended_reps_final, self.context_mask)
 
 
     def add_loss(self):
@@ -254,11 +254,11 @@ class QAModel(object):
             tf.summary.scalar('loss_end', self.loss_end)
 
             # calculate L2 loss
-            reg_variables = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-            loss_reg = tf.contrib.layers.apply_regularization(regularizer, reg_variables)
+            # reg_variables = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+            # loss_reg = tf.contrib.layers.apply_regularization(regularizer, reg_variables)
 
             # Add the two losses
-            self.loss = self.loss_start + self.loss_end + loss_reg
+            self.loss = self.loss_start + self.loss_end #+ loss_reg
             tf.summary.scalar('loss', self.loss)
 
 
